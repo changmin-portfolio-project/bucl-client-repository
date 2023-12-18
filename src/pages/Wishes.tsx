5f5334e18a6a6c9c68cac4585ac9615dab72f16d
taewok <taewok5163@naver.com>
2023-12-18 18:32:38 +0900

    refactor: 필요없는 함수 삭제

--- src/pages/Wishes.tsx
+++ src/pages/Wishes.tsx
@@ -5,10 +5,6 @@ import Body from '../conponents/wish/Body';
 import Header from '../conponents/wish/Header';
 
 const WishesPage: React.FC = () => {
-  const openPopup = (url: string) => {
-    window.open(url, '_blank', 'width=400,height=800');
-    // '_blank'은 새 창에 열기, 'width'와 'height'는 창의 크기를 지정합니다.
-  };
   return (
     <WishesPageContainer>
       <Header />
