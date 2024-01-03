fe52c15dc10628fc01ea1eab76a55cf38901338f
taewok <taewok51615@gmail.com>
2024-01-03 16:08:47 +0900

    feat: 고객센터 페이지 퍼블리싱 완료 (#28)
    * feat: 고객센터 페이지 퍼블리싱 완료
    
    * feat: 회원 탈퇴 api 개발 및 적용
    
    ---------
    
    Co-authored-by: Changmin Kim <ckd29672@naver.com>
    Co-authored-by: Matt <59727077+changminkim-329@users.noreply.github.com>

---
+++ src/conponents/customer/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import HeaderLayout from '../layout/HeaderLayout';
+import PrevBtn from '../PrevBtn';
+import styled from 'styled-components';
+
+const Header: React.FC = () => {
+  const HeaderLayoutStyle: React.CSSProperties = {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  };
+  return (
+    <HeaderLayout style={HeaderLayoutStyle}>
+      <Title>
+        <PrevBtn />
+        고객센터
+      </Title>
+    </HeaderLayout>
+  );
+};
+
+const Title = styled.h2`
+  text-align: center;
+  font: ${({ theme }) => theme.fontSizes.Subhead2};
+`;
+
+export default Header;
