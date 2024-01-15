e08e49357aa4cac96f7c72b981827b3cebb834e2
taewok <taewok5163@naver.com>
2024-01-15 19:19:22 +0900

    feat: 홈 페이지 진입시 찜리스트 함수 실행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TabBar from '../conponents/TabBar';
 import styled from 'styled-components';
 import Header from '../conponents/home/Header';
 import TopBtn from '../conponents/TopBtn';
 import Body from '../conponents/home/Body';
+import { wishListAtom } from '../states/productAtom';
+import { useSetRecoilState } from 'recoil';
+import { getWishList } from '../services/wish/getWishList';
 
 const HomePage: React.FC = () => {
+  const setWishList = useSetRecoilState(wishListAtom);
+  useEffect(() => {
+    getWishList().then((res) => {
+      setWishList(res.data);
+    });
+  }, []);
   return (
     <Container>
       <Header />
